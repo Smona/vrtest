1d235af2e226fd2f8ab8fd1f869eba217e4751df
Mason Bourgeois <mason.bourgeois@gmail.com>
2016-09-07 14:33:24 -0500

    Add ability to load album art

--- SCAPI/js/soundcloud.js
+++ SCAPI/js/soundcloud.js
@@ -2,28 +2,37 @@
 $(function () {
 	SC.initialize({client_id: '28b05a26f8f8bec9d27bea7c938337ff'});
 	getTracks();
-	var timer = setInterval(getTracks, 10000);
+	var timer = setInterval(getTracks, 100000);
 })
 
 function getTracks() {
 	$('ul').html('');
 	SC.get('/tracks')
 	.then(function (response) {
+		console.log(response);
 		response.forEach(function (track) {
 			$('ul').append('<li>' + track.created_at + ' - <a data-id="' + track.id + '" href="#">' + track.title + '</a></li>');
 		});
 		$('li a').on('click', function () {
 			loadTrack($(this).attr('data-id'));
 		});
+		console.log(response[0].artwork_url);
+		// $('.art-display').css('background-image', 'url(' + response[0].artwork_url + ')');
 	});
 }
 
-function loadTrack(track) {
-	console.log('Track loaded:' + track);
+function loadTrack(trackId) {
 	var stream;
-	SC.stream('/tracks/' + track).then(function(player){
+	SC.get('/tracks/' + trackId).then(function (resource) {
+		if (resource.artwork_url) {
+			$('.art-display').css('background-image', 'url(' + resource.artwork_url + ')');
+		}
+	});
+	// console.log(SC.get('/tracks/' + trackId + '/artwork_url'));
+	SC.stream('/tracks/' + trackId).then(function(player){
 		stream = player;
 		stream.play();
 	});
+	console.log('Track loaded:' + trackId);
 	return stream;
 }
